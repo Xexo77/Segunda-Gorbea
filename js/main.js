aab4bcaef1b820b2cb420c6af6ad427c63a5a33c
Xexo77 <sergio92checho@gmail.com>
2025-09-24 09:15:20 -0300

    Create main.js

---
+++ js/main.js
@@ -0,0 +1,8 @@
+// Menú hamburguesa
+const hamburger = document.querySelector('.hamburger');
+const navMenu = document.querySelector('.nav-menu');
+
+hamburger.addEventListener('click', () => {
+  navMenu.classList.toggle('active');
+  hamburger.classList.toggle('active');
+});
